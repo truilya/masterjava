1730bc6e08753d7b58508b698b2363b34febce73
Ilya Trukhonin <inc@bk.ru>
2020-08-02 01:13:43 +0500

    StAX implementation

--- src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
+++ src/main/java/ru/javaops/masterjava/xml/util/StaxStreamProcessor.java
@@ -1,22 +1,35 @@
 package ru.javaops.masterjava.xml.util;
 
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class StaxStreamProcessor implements AutoCloseable {
     private static final XMLInputFactory FACTORY = XMLInputFactory.newInstance();
 
     private final XMLStreamReader reader;
+    private XMLEventReader eventReader;
 
     public StaxStreamProcessor(InputStream is) throws XMLStreamException {
         reader = FACTORY.createXMLStreamReader(is);
+        eventReader = FACTORY.createXMLEventReader(reader);
     }
 
     public XMLStreamReader getReader() {
         return reader;
+    }
+
+    public XMLEventReader getEventReader() {
+        return eventReader;
     }
 
     public boolean doUntil(int stopEvent, String value) throws XMLStreamException {
@@ -36,11 +49,26 @@ public class StaxStreamProcessor implements AutoCloseable {
     }
 
     public String getElementValue(String element) throws XMLStreamException {
-        return doUntil(XMLEvent.START_ELEMENT, element) ? reader.getElementText() : null;
+        return doUntil(XMLEvent.START_ELEMENT, element) ? reader.getLocalName() : null;
     }
 
     public String getText() throws XMLStreamException {
         return reader.getElementText();
+    }
+
+    public boolean startElement(String element, String parent) throws XMLStreamException {
+        while (reader.hasNext()) {
+            int event = reader.next();
+            if (parent != null && event == XMLEvent.END_ELEMENT &&
+                    parent.equals(reader.getLocalName())) {
+                return false;
+            }
+            if (event == XMLEvent.START_ELEMENT &&
+                    element.equals(reader.getLocalName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
@@ -52,5 +80,50 @@ public class StaxStreamProcessor implements AutoCloseable {
                 // empty
             }
         }
+        if (eventReader != null) {
+            try {
+                eventReader.close();
+            } catch (XMLStreamException e) {
+                // empty
+            }
+        }
     }
+
+    public boolean checkElementByNameAndAttrValue(XMLEvent event, String name, String attrName, final String value) throws Exception {
+        if (event.isStartElement() && name.equals(event.asStartElement().getName().getLocalPart())) {
+            Attribute attribute = getAttributeByName(event.asStartElement(), attrName);
+            if (attribute!= null && value.equals(attribute.getValue())){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public Attribute getAttributeByName(StartElement startElement, String attrName){
+        Iterator<Attribute> iterator = startElement.getAttributes();
+        while (iterator.hasNext()){
+                Attribute attribute = iterator.next();
+                if (attrName.equals(attribute.getName().getLocalPart())){
+                    return attribute;
+                }
+            }
+        return null;
+    }
+
+    public List<String> getElementValuesInsideParent(String parent,String elem) throws XMLStreamException{
+        XMLEvent event = eventReader.nextEvent();
+        List<String> result = new ArrayList<>();
+        while (eventReader.hasNext() && !(event.isEndElement() && parent.equals(event.asEndElement().getName().getLocalPart()))){
+            if (event.isStartElement() && elem.equals(event.asStartElement().getName().getLocalPart())){
+                event = eventReader.nextEvent();
+                if (event.isCharacters()){
+                    result.add(event.asCharacters().getData());
+                }
+            }
+            event = eventReader.nextEvent();
+        }
+        return result;
+    }
+
 }
--- src/test/java/MainXml.java
+++ src/test/java/MainXml.java
@@ -2,13 +2,20 @@ import com.google.common.io.Resources;
 import ru.javaops.masterjava.xml.schema.*;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.XMLEvent;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class MainXml {
 
     private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+    private static final String PROJECT_ELEMENT = "Project";
+    private static final String PROJECT_USERS = "Users";
+    private static final String USER_ELEMENT = "User";
+
 
     static {
         JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
@@ -27,9 +34,13 @@ public class MainXml {
             System.out.println(s);
         }
         System.out.println("\n==== by Stax ====");
+        users = getUsersByProjectSTAX("masterjava");
+        for (String s : users) {
+            System.out.println(s);
+        }
     }
 
-    public static Set<String> getUsersByProjectJAXB(String projectName) throws Exception {
+    private static Set<String> getUsersByProjectJAXB(String projectName) throws Exception {
         Payload payload = JAXB_PARSER.unmarshal(
                 Resources.getResource("payload.xml").openStream());
         List<Project> projects = payload.getProjects().getProject();
@@ -46,6 +57,33 @@ public class MainXml {
                 }
                 return result;
             }
+        }
+        return result;
+    }
+
+    private static Set<String> getUsersByProjectSTAX(final String projectName) throws Exception {
+        Set<String> result = new TreeSet<>();
+        Set<String> projectUsers = new TreeSet<>();
+        Map<String,String> users = new HashMap<>();
+        try (StaxStreamProcessor processor = new StaxStreamProcessor(Resources.getResource("payload.xml").openStream())) {
+            XMLEventReader eventReader = processor.getEventReader();
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (processor.checkElementByNameAndAttrValue(event, PROJECT_ELEMENT, "name", projectName)) {
+                    List<String> localUsers = processor.getElementValuesInsideParent(PROJECT_ELEMENT, PROJECT_USERS);
+                    for(String s : localUsers){
+                        projectUsers.addAll(Arrays.asList(s.split(" ")));
+                    }
+                } else if (event.isStartElement() && USER_ELEMENT.equals(event.asStartElement().getName().getLocalPart())){
+                    Attribute attribute =  processor.getAttributeByName(event.asStartElement(),"login");
+                    String sLogin = attribute.getValue();
+                    String fullName = processor.getElementValuesInsideParent(USER_ELEMENT, "fullName").get(0);
+                    users.put(sLogin,fullName);
+                }
+            }
+        }
+        for (String s :projectUsers){
+            result.add(users.get(s));
         }
         return result;
     }
