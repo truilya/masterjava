a8affb4d2e5e1115d1c7d085b30a18b420786d48
Ilya Trukhonin <inc@bk.ru>
2020-10-06 23:58:54 +0500

    my migrate settings

--- sql/lb_apply.bat
+++ sql/lb_apply.bat
@@ -1,8 +1,9 @@
-set LB_HOME=c:\java\liquibase-3.5.3
+set LB_HOME=c:\java\liquibase
 call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
 --classpath=%LB_HOME%\lib ^
---changeLogFile=databaseChangeLog.sql ^
+--changeLogFile=C:\JavaProjects\masterjava\sql\databaseChangeLog.sql ^
 --url="jdbc:postgresql://localhost:5432/masterjava" ^
 --username=user ^
 --password=password ^
+--logLevel=debug ^
 migrate
